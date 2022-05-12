835aa4c869decc3b1ed0dc4cfab7d18b0ed0498a
Liangxi Liu <liangxi.liu666@gmail.com>
2022-05-12 13:12:19 +0100

    delete token

--- code/app.js
+++ code/app.js
@@ -25,30 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false, // don't save session if unmodified
-  saveUninitialized: true, // don't create session until something stored
-  // store: new SQLiteStore({ db: 'sessions.db', dir: './var/db' })
-}));
-app.use(csrf());
-app.use(passport.authenticate('session'));
-app.use(function(req, res, next) {
-  var msgs = req.session.messages || [];
-  res.locals.messages = msgs;
-  res.locals.hasMessages = !! msgs.length;
-  req.session.messages = [];
-  next();
-});
-app.use(function(req, res, next) {
-  res.locals.csrfToken = req.csrfToken();
-  next();
-});
+
 
 app.use('/', index);
 app.use('/', users);
