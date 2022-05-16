ec2285acdb060de460bddd8942a9c58ac1d43606
Bowen Zhang <bzhang73@sheffield.ac.uk>
2022-05-16 05:12:49 +0100

    Code optimization

--- code/socket.io/socket-io.js
+++ code/socket.io/socket-io.js
@@ -23,25 +23,15 @@ exports.init = function(io) {
             socket.broadcast.to(room).emit('draw', room, userId, width, height, prevX, prevY, currX, currY, color, thickness);
           });
 
-<<<<<<< HEAD
-      socket.on('draw', function (room, userId, width,height, prevX, prevY, currX, currY, color, thickness) {
-        socket.broadcast.to(room).emit('draw', room, userId, width, height, prevX, prevY, currX, currY, color, thickness);
-      });
-
-      socket.on('disconnect', function(){
-        console.log('someone disconnected');
-=======
           socket.on('disconnect', function(){
             console.log('someone disconnected');
           });
         } catch (e) {
         }
->>>>>>> feature/work-offline
       });
 
   // the news namespace
   const news= io
-<<<<<<< HEAD
         .of('/news')
         .on('connection', function (socket) {
       try {
@@ -59,23 +49,4 @@ exports.init = function(io) {
       } catch (e) {
       }
     });
-=======
-      .of('/news')
-      .on('connection', function (socket) {
-        try {
-          /**
-           * it creates or joins a room
-           */
-          socket.on('create or join', function (room, userId) {
-            socket.join(room);
-            socket.broadcast.to(room).emit('joined', room, userId);
-          });
-
-          socket.on('disconnect', function(){
-            console.log('someone disconnected');
-          });
-        } catch (e) {
-        }
-      });
->>>>>>> feature/work-offline
 }
